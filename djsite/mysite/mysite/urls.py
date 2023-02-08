2ee8bd1158fe36e2f8f10c591eed0cb299c5f1c1
Sergiy Zhyla <Ibanusia@gmail.com>
2023-02-08 20:19:33 +0200

    delete db and cache

--- djsite/mysite/mysite/urls.py
+++ djsite/mysite/mysite/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-# from task.views import task_list_view, task_details_view
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
